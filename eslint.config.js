09b3f6e7171d9c8a713df5ff4431ab88133ba8cc
unknown <arthur.saussereau@hotmail.fr>
2026-01-05 23:20:59 +0100

    added Prettier and config for ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
+
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+
+      // 🔥 TOUJOURS EN DERNIER
+      prettierConfig,
     ],
+
+    plugins: {
+      prettier,
+    },
+
+    rules: {
+      // 🔥 Prettier devient une règle ESLint
+      'prettier/prettier': 'warn',
+    },
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+]);
